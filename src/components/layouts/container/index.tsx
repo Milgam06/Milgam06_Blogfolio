47f0339f21c0f141718c4177bc85ba7873526539
Milgam06 <milgamfruit@gmail.com>
2024-05-16 22:17:50 +0900

    feat(component): add styles on SectionLayout

--- src/components/layouts/container/index.tsx
+++ src/components/layouts/container/index.tsx
@@ -4,7 +4,6 @@ export const Container = styled.div`
   width: 100%;
   max-width: 1140px;
   margin: 0 auto;
-  border: 1px solid red;
   @media screen and (max-width: 991px) {
     padding-left: 3.2rem;
     padding-right: 3.2rem;
--- src/components/layouts/index.ts
+++ src/components/layouts/index.ts
@@ -1,2 +1,3 @@
 export * from "./DefaultLayout";
+export * from "./SectionLayout";
 export * from "./Container";
