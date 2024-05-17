fc1a177dd41eca5375bc229ee440a3548671eb15
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-05-18 01:27:29 +0900

    feat(component): style about Text color

--- src/components/main/MainSection/index.tsx
+++ src/components/main/MainSection/index.tsx
@@ -1,4 +1,4 @@
-import { SectionLayout } from "@/components";
+import { SectionLayout, Text } from "@/components";
 
 export const MainSection: React.FC = () => {
   return (
@@ -6,7 +6,15 @@ export const MainSection: React.FC = () => {
       <SectionLayout
         title={
           <span>
-            제 이름은 <span style={{ color: "red" }}>박여준</span>입니다.
+            제 이름은{" "}
+            <Text
+              size={6}
+              weight={900}
+              color="linear-gradient(to right, #A871FD,#021AA6)"
+            >
+              박여준
+            </Text>
+            입니다
           </span>
         }
         subtitle="소통하며 생각하는 개발자"
