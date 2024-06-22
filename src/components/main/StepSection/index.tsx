79a496e1e9380e56e9c716a2a8525c3a5434557a
Milgam06 <milgamfruit@gmail.com>
2024-06-22 14:00:49 +0900

    fix(component): rename props name

--- src/components/main/StepSection/index.tsx
+++ src/components/main/StepSection/index.tsx
@@ -1,51 +1,54 @@
-import { SectionLayout, Text, Button } from "@/components";
+import { SectionLayout, Button, StepModal } from "@/components";
 import { useFadeInScroll } from "@/hooks";
+import { useModal } from "@/providers";
 
 import * as S from "./styled";
 
 export const StepSection: React.FC = () => {
+  const { open } = useModal();
   const { fadeInScroll } = useFadeInScroll();
   const onClick = () => {
-    console.log("clicked");
+    open({ children: <StepModal /> });
+    console.log("asdf");
   };
   return (
     <>
       <SectionLayout>
-        <S.StepSectionContentContainer {...fadeInScroll({ delay: 2 })}>
-          <Button scale={true} onClickHandler={onClick}>
+        <S.StepSectionContentContainer {...fadeInScroll({ delay: 0.1 })}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
-          <Button scale={true} onClickHandler={onClick}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
-          <Button scale={true} onClickHandler={onClick}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
-          <Button scale={true} onClickHandler={onClick}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
-          <Button scale={true} onClickHandler={onClick}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
-          <Button scale={true} onClickHandler={onClick}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
-          <Button scale={true} onClickHandler={onClick}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
-          <Button scale={true} onClickHandler={onClick}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
-          <Button scale={true} onClickHandler={onClick}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
-          <Button scale={true} onClickHandler={onClick}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
-          <Button scale={true} onClickHandler={onClick}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
-          <Button scale={true} onClickHandler={onClick}>
+          <Button isLarge={true} onClickHandler={onClick}>
             asdf
           </Button>
         </S.StepSectionContentContainer>
