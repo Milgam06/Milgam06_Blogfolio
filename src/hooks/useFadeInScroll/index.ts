958516402512065c79a51ae51db140b8f6c4885b
Milgam06 <milgamfruit@gmail.com>
2024-06-22 13:56:22 +0900

    fix(hook): return issue

--- src/hooks/useFadeInScroll/index.ts
+++ src/hooks/useFadeInScroll/index.ts
@@ -3,13 +3,12 @@ import { MotionProps } from "framer-motion";
 
 export const useFadeInScroll = () => {
   const fadeInScroll = useCallback(({ delay }: { delay: number }) => {
-    ({
+    return {
       initial: { opacity: 0, transform: "translate3d(0, 50%, 0)" },
       whileInView: { opacity: 1, transform: "translate3d(0, 0, 0)" },
       transition: { ease: [0, 0, 0.2, 1], duration: 0.7, delay },
       viewport: { once: true },
-    }) as MotionProps;
+    } as MotionProps;
   }, []);
-
   return { fadeInScroll };
 };
