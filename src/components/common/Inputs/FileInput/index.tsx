b41db3b238ccf0adf4e00c859525d41d461f8b88
Milgam06 <milgamfruit@gmail.com>
2024-07-04 23:09:04 +0900

    chore(component): renamed InputText folder and make FileInput folder

---
+++ src/components/common/Inputs/FileInput/index.tsx
@@ -0,0 +1,13 @@
+export interface UploadFileProps {
+  filename: string;
+  size: string;
+  fileData: string;
+}
+
+export const FileUploadInput: React.FC = () => {
+  return (
+    <>
+      <h1>asdf</h1>
+    </>
+  );
+};
