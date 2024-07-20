ee45a501c97b4beb3976f88aacb7ed9260c08a90
Milgam06 <milgamfruit@gmail.com>
2024-07-21 02:20:51 +0900

    feat(component): add react-dropzone

--- src/components/common/Modal/AddModal/index.tsx
+++ src/components/common/Modal/AddModal/index.tsx
@@ -1,8 +1,13 @@
+import DropZone from "react-dropzone";
+
 import { Text, TextArea, Input } from "@/components";
 
 import * as S from "./styled";
 
 export const AddModal: React.FC = () => {
+  const onDropHandler = (acceptedFiles: File[]) => {
+    console.log(acceptedFiles);
+  };
   return (
     <>
       <S.AddModalContainer>
@@ -15,6 +20,27 @@ export const AddModal: React.FC = () => {
             Step의 세부 내용을 적어주세요.
           </Text>
           <TextArea />
+          <Text size={1} weight={600}>
+            Step과 관련된 이미지를 업로드해주세요.
+          </Text>
+          <DropZone
+            onDrop={onDropHandler}
+            accept={{ "image/*": [".jpg", ".jpeg", ".png"] }}
+            minSize={1024}
+            maxSize={3272000}
+          >
+            {({ getRootProps, getInputProps }) => (
+              <section>
+                <div
+                  style={{ border: "1px solid red" }}
+                  {...getRootProps({ className: "dropzone" })}
+                >
+                  <input {...getInputProps()} />
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                </div>
+              </section>
+            )}
+          </DropZone>
         </S.AddModalContentContainer>
       </S.AddModalContainer>
     </>
