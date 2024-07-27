e7c3fb1e655adc6991d1cefd8177dbfee55ffa53
Milgam06 <milgamfruit@gmail.com>
2024-07-27 22:31:16 +0900

    feat(component): make Button to show title of step

--- src/components/main/StepSection/index.tsx
+++ src/components/main/StepSection/index.tsx
@@ -1,15 +1,28 @@
+import { useEffect, useState } from "react";
+
 import { SectionLayout, Button, StepModal, AddModal, Text } from "@/components";
 import { useFadeInScroll } from "@/hooks";
 import { useModal } from "@/providers";
+import { useStoryStore, StoryResponeProps } from "@/hooks";
 
 import * as S from "./styled";
 
 export const StepSection: React.FC = () => {
+  const { getAllStories } = useStoryStore();
   const { open } = useModal();
   const { fadeInScroll } = useFadeInScroll();
+  const [stories, setStories] = useState<StoryResponeProps[]>([]);
+
+  useEffect(() => {
+    const fetchStories = async () => {
+      const storedStories = await getAllStories();
+      setStories(storedStories);
+    };
+    fetchStories();
+  }, []);
 
   const onClick = () => {
-    open({ children: <StepModal /> });
+    // open({ children: <StepModal /> });
     console.log("asdf");
   };
   const onOpenAddModal = () => {
@@ -19,42 +32,11 @@ export const StepSection: React.FC = () => {
     <>
       <SectionLayout title="Exerience" subtitle="저의 소중한 경험들이에요.">
         <S.StepSectionContentContainer {...fadeInScroll({ delay: 0.08 })}>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
-          <Button isLarge={true} onClickHandler={onClick}>
-            asdf
-          </Button>
+          {stories.map((story) => (
+            <Button isLarge={true} onClickHandler={onClick}>
+              {story.title}
+            </Button>
+          ))}
           <Button isLarge={true} onClickHandler={onOpenAddModal}>
             +
           </Button>
