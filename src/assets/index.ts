3c38301322fe8a1266918acbaf87b22e018b62ab
Milgam06 <milgamfruit@gmail.com>
2024-07-31 20:53:16 +0900

    feat(asset): add Loading Img

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,5 +4,7 @@ import NewJ3 from "./react진스_2 복사본.png";
 import NewJ4 from "./react진스_3 복사본.png";
 import NewJ5 from "./맥북진스 복사본.png";
 import NewJ6 from "./맥북진스_3 복사본.png";
+import LoadingImg from "./LoadingImg.png";
+import LoadingImg2 from "./LoadingImg2.png";
 
-export { NewJ, NewJ2, NewJ3, NewJ4, NewJ5, NewJ6 };
+export { NewJ, NewJ2, NewJ3, NewJ4, NewJ5, NewJ6, LoadingImg, LoadingImg2 };
