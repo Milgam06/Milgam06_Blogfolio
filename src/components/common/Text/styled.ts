309f568db500ad939dc4594fb2a1caa06cad33d4
Milgam06 <milgamfruit@gmail.com>
2024-07-31 14:08:44 +0900

    feat(component): change Text's style to show text includes \n

--- src/components/common/Text/styled.ts
+++ src/components/common/Text/styled.ts
@@ -5,10 +5,11 @@ import { TextProps } from "./index";
 export const TextElement = styled.span<TextProps>`
   font-size: ${(props) => props.size}rem;
   font-weight: ${(props) => props.weight};
-  text-align: center;
   background: ${(props) => (props.color ? props.color : "#000")};
   background-clip: text;
   -webkit-background-clip: text;
   color: transparent;
   border: none;
+  white-space: pre-line;
+  text-align: left;
 `;
