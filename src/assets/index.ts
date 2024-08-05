8e7867345af9c25a0071fa97247032fcf58c9da2
Milgam06 <milgamfruit@gmail.com>
2024-08-05 14:07:24 +0900

    chore(asset): add my Img

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,5 +6,16 @@ import NewJ5 from "./맥북진스 복사본.png";
 import NewJ6 from "./맥북진스_3 복사본.png";
 import LoadingImg from "./LoadingImg.png";
 import LoadingImg2 from "./LoadingImg2.png";
+import WhoamiImg from "./whoami.png";
 
-export { NewJ, NewJ2, NewJ3, NewJ4, NewJ5, NewJ6, LoadingImg, LoadingImg2 };
+export {
+  NewJ,
+  NewJ2,
+  NewJ3,
+  NewJ4,
+  NewJ5,
+  NewJ6,
+  LoadingImg,
+  LoadingImg2,
+  WhoamiImg,
+};
