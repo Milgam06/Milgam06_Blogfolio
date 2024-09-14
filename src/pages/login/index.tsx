9fe60175b04ec446b51b4f4e49f8c242b0acf007
Milgam06 <milgamfruit@gmail.com>
2024-09-14 23:49:08 +0900

    feat(page): add LoginPage

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,18 @@
+import { Text } from "@/components";
+import { useLogin } from "@/hooks";
+
 export const LoginPage: React.FC = () => {
+  const { handleWhoAmI } = useLogin();
   return (
     <>
-      <h1>asdf</h1>
+      <Text
+        size={3}
+        weight={900}
+        color="linear-gradient(to right, #cba8ff,#0e2ee2)"
+      >
+        혹시 박여준이십니까?
+      </Text>
+      <button onClick={handleWhoAmI}>로그인</button>
     </>
   );
 };
