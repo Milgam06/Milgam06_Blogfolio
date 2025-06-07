0f1ceac2e4260bd06827a634ba17597cdb35eef9
Milgam06 <milgamfruit@gmail.com>
2025-06-08 03:19:58 +0900

    fix: delete console.log

--- src/components/main/HighlightSection/index.tsx
+++ src/components/main/HighlightSection/index.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import { useColor } from "color-thief-react";
+import { useState } from 'react';
+import { useColor } from 'color-thief-react';
 
-import { SectionLayout, Text } from "@/components";
-import { useFadeInScroll, StoryProps } from "@/hooks";
+import { SectionLayout, Text } from '@/components';
+import { useFadeInScroll, StoryProps } from '@/hooks';
 
-import * as S from "./styled";
-import { useEffect } from "react";
+import * as S from './styled';
+import { useEffect } from 'react';
 
 export interface HighlightSectionProps extends StoryProps {
   isFirst: boolean;
@@ -16,36 +16,24 @@ export interface HighlightSectionProps extends StoryProps {
  * @param content: highlight section content
  * @param filesUrl: highlight section image url
  */
-export const HighlightSection: React.FC<HighlightSectionProps> = ({
-  title,
-  content,
-  filesUrl,
-  isFirst,
-}) => {
-  const [basicColor, setBasicColor] = useState<string>("#fefefe  ");
+export const HighlightSection: React.FC<HighlightSectionProps> = ({ title, content, filesUrl, isFirst }) => {
+  const [basicColor, setBasicColor] = useState<string>('#fefefe  ');
   const { fadeInScroll } = useFadeInScroll();
   const thumbnailImg = filesUrl[1];
 
-  const { data, loading } = useColor(thumbnailImg, "hex");
+  const { data, loading } = useColor(thumbnailImg, 'hex');
   useEffect(() => {
-    console.log(data, "data");
     if (!loading && data !== undefined) {
       setBasicColor(data);
     }
-    console.log(basicColor, "basicColor");
   }, [data]);
 
   return (
     <>
       <SectionLayout fullScreen={true}>
         {!loading ? (
-          <S.HighlightSectionContainer
-            backgroundColor={basicColor}
-            isFirst={isFirst}
-          >
-            <S.HighlightSectionContentContainer
-              {...fadeInScroll({ delay: 0.06 })}
-            >
+          <S.HighlightSectionContainer backgroundColor={basicColor} isFirst={isFirst}>
+            <S.HighlightSectionContentContainer {...fadeInScroll({ delay: 0.06 })}>
               <S.HighlightSectionImageBanner>
                 {filesUrl.slice(0, 4).map((img, i) => (
                   <S.HighlightSectionImage
