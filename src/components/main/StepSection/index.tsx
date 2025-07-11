e573a3358637a38302d8669504b978f13f1e4cee
MILGAM <93873127+Milgam06@users.noreply.github.com>
2025-07-11 13:02:26 +0900

    feat: standardize stepSection's shape

--- src/components/main/StepSection/index.tsx
+++ src/components/main/StepSection/index.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react';
 
-import { SectionLayout, StepButton, StepModal, AddModal, Text, BaseLayout } from '@/components';
+import { StepButton, StepModal, AddModal, Text, BaseLayout } from '@/components';
 import { useFadeInScroll } from '@/hooks';
 import { useModal } from '@/providers';
 import { useStoryStore, StoryResponseProps } from '@/hooks';
 import { useGlobalStore } from '@/store/useGlobalStore';
 
-import * as S from './styled';
 import { Flex } from '@mantine/core';
 
 export const StepSection: React.FC = () => {
@@ -34,21 +33,52 @@ export const StepSection: React.FC = () => {
   };
   return (
     <>
-      <BaseLayout justify="center" align="center" isFullWidth gap={100}>
+      <BaseLayout justify="center" align="center" gap={100}>
         <Text size={8} weight={900} color="linear-gradient(to right, #FF8C42 0%, #FFE5B4 50%,#FF8C42 100%)">
           My Projects
         </Text>
-        <Flex justify="flex-start" align="center" gap="xl">
+        <Flex
+          w="100%"
+          justify="flex-start"
+          align="center"
+          gap="xl"
+          p="xl"
+          styles={{
+            root: {
+              overflow: 'scroll',
+              scrollbarWidth: 'none',
+            },
+          }}>
           {stories.map((story) => (
-            <StepButton
-              isLarge={true}
-              hasImage={true}
-              imgSrc={story.filesUrl[0]}
-              onClickHandler={() => onOpenStepModal(story.id)}>
-              <Text size={1.4} weight={100}>
-                {story.title}
-              </Text>
-            </StepButton>
+            <>
+              <StepButton
+                isLarge={true}
+                hasImage={true}
+                imgSrc={story.filesUrl[0]}
+                onClickHandler={() => onOpenStepModal(story.id)}>
+                <Text size={1.4} weight={100}>
+                  {story.title}
+                </Text>
+              </StepButton>
+              <StepButton
+                isLarge={true}
+                hasImage={true}
+                imgSrc={story.filesUrl[0]}
+                onClickHandler={() => onOpenStepModal(story.id)}>
+                <Text size={1.4} weight={100}>
+                  {story.title}
+                </Text>
+              </StepButton>
+              <StepButton
+                isLarge={true}
+                hasImage={true}
+                imgSrc={story.filesUrl[0]}
+                onClickHandler={() => onOpenStepModal(story.id)}>
+                <Text size={1.4} weight={100}>
+                  {story.title}
+                </Text>
+              </StepButton>
+            </>
           ))}
           {isLoggedIn && (
             <StepButton isLarge={false} onClickHandler={onOpenAddModal}>
