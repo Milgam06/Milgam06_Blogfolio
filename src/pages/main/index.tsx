0f3254f5017b95d55ade509af22fc4f5d0a8b42e
Milgam06 <milgamfruit@gmail.com>
2025-08-18 14:58:43 +0900

    feat: add SegmentType's pages

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,11 +1,19 @@
-import { MainSection, StepSection, IntroduceSection, TechStackSection } from '@/components';
+import { MainSection, StepSection, IntroduceSection, TechStackSection, Text } from '@/components';
 import { useLogin } from '@/hooks';
+import { useContentTypeStore } from '@/store';
 import { Stack } from '@mantine/core';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useDidMount } from 'rooks';
 
 export const MainPage: React.FC = () => {
+  const { contentType } = useContentTypeStore();
   const { handleValidateUser } = useLogin();
+
+  const { isContentTypeBlog, isContentTypePortfolio } = useMemo(() => {
+    const isContentTypeBlog = contentType === 'BLOG';
+    const isContentTypePortfolio = contentType === 'PORTFOLIO';
+    return { isContentTypeBlog, isContentTypePortfolio };
+  }, [contentType]);
 
   const getSessionData = useCallback(() => {
     const sessionData = sessionStorage.getItem(`firebase:authUser:${import.meta.env.VITE_FIREBASE_API_KEY}:[DEFAULT]`);
@@ -28,11 +36,22 @@ export const MainPage: React.FC = () => {
   });
 
   return (
-    <Stack>
-      <MainSection />
-      <IntroduceSection />
-      <TechStackSection />
-      <StepSection />
-    </Stack>
+    <>
+      {isContentTypeBlog && (
+        <Stack w="100%" h="100%" justify="center" align="center">
+          <Text size={10} weight={900}>
+            Sorry, It's not ready
+          </Text>
+        </Stack>
+      )}
+      {isContentTypePortfolio && (
+        <Stack>
+          <MainSection />
+          <IntroduceSection />
+          <TechStackSection />
+          <StepSection />
+        </Stack>
+      )}
+    </>
   );
 };
